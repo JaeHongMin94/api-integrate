c8d1b9cd87f1d39d303240237824fa945d3ff422
JaeHongMin94 <moonshin123@naver.com>
2021-01-18 20:11:27 +0900

    async 커스텀 hook 만들어서 사용하기

--- src/Users.js
+++ src/Users.js
@@ -1,45 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
+import User from './User';
+
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users/'
+  );
+  return response.data;
+}
 
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
+  const [state, refetch] = useAsync(getUsers, [], true);
+  const [userId, setUserId] = useState(null);
 
-  const fetchUsers = async () => {
-    try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users/'
-      );
-      setUsers(response.data);
-    } catch (e) {
-      console.log('Error code : ', e.response.status);
-      setError(e);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
+  const { loading, data: users, error } = state;
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
-  if (!users) return null;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
 
   return (
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name});
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
